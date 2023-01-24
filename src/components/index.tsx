99affaa70e199cb9a8687888615de95ed2e4ae2d
Flavio Leonardo Machado <60984765+LeonardoMachado30@users.noreply.github.com>
2023-01-24 16:20:15 -0300

    Revert "ADD: Project into fork"

--- src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import Header from "./Header/Header";
-import CardList from "./CardList/CardList";
-import ShoppingCart from "./ShoppingCart/ShoppingCart";
-import Footer from "./Footer/Footer";
-
-export { CardList, Header, Footer, ShoppingCart };
